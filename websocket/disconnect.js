c5fa19e983ead2886e234183dae8ca0dceac5c4e
araooz <fali.araoz@utec.edu.pe>
2025-11-16 17:33:36 -0500

    refactor: enhance methodArn handling in autorizer and remove unused WebSocket files

--- websocket/disconnect.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async (event) => {
-  await ddb.send(new DeleteItemCommand({
-    TableName: process.env.CONNECTIONS_TABLE,
-    Key: { connectionId: { S: event.requestContext.connectionId } }
-  }));
-
-  return { statusCode: 200 };
-};
